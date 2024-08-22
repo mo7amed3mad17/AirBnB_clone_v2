344d75f68c4134c5238cec906100e73a2cc3feb0
mo7amed3mad17 <memo12317emad@gmail.com>
2024-08-22 12:38:16 +0200

    Try

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, DateTime
 from datetime import datetime
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
